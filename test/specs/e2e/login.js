750f51398f24553cbf5063a9a5d4b953c007e75e
Bradley Barnes <bradleybarnes@northwesternmutual.com>
2022-12-12 13:19:07 -0600

    Solution for login test

--- test/specs/e2e/login.js
+++ test/specs/e2e/login.js
@@ -5,12 +5,12 @@ describe("Login Flow", () => {
 
    it("Should login with invalid username and password", async () => {
     await browser.waitAndClick('#signin_button')
-    await $('#login_form').awaitForDisplay()
+    await $('#login_form').waitForDisplayed()
     await (await $('#user_login')).setValue('test')
-    await (await $('#user_passwor')).setValue('test')
+    await (await $('#user_password')).setValue('test')
     await (await $('input[type="submit"]')).click()
     const errorMessage = await $('.alert-error')
-    await expect(errorMessage).toHaveTextContainning(
+    await expect(errorMessage).toHaveTextContaining(
         'Login and/or password are wrong'
     )
    }) 
@@ -18,11 +18,13 @@ describe("Login Flow", () => {
    it('Reset Account Password', async () => {
     const email = 'test@test.com'
 
-    await browser.waitAndClick('*=Forget')
-    await (await $('#user_email')).awaitForDisplay()
-    await (await $('#user_email')).setValue(email)
+    await browser.waitAndClick('a[href="/forgot-password.html"]')
+    const input = await $('#user_email');
+    await input.waitForDisplayed();
+    await input.setValue(email);
+    await $('input[type="submit"]').click();
   
     const message = await $('.span6')
-    await expect(message).toHaveTextContaining('email: ${email}')
+    await expect(message).toHaveTextContaining(`email: ${email}`)
    })
 })
