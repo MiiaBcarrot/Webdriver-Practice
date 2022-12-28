66a6f383d8d2ce5c308178e3be673c0ae58f9852
MiiaBcarrot <minouepaul123@gmail.com>
2022-12-28 11:04:59 -0600

    fix stuff

--- test/specs/e2e/product-order.js
+++ test/specs/e2e/product-order.js
@@ -10,10 +10,36 @@ describe('Sauce Demo - Product Order', () => {
         await browser.sauceLogout()
     })
     it('Should complete product order', async () => {
-        await browser.pause(300)
+        // await browser.pause(300)
         // Put product into shopping cart
+        await (await $('#inventory_container')).waitForDisplayed()
+        await (await $('[data-test="add-to-cart-sauce-labs-backpack"]')).click()
+        await (await $('[data-test="add-to-cart-sauce-labs-bike-light"]')).click()
+        await (await $('.shopping_cart_link')).click()
+
         // Assert shopping cart
+        await (await $('.cart_list')).waitForDisplayed()
+        await (await $('[data-test="checkout"]')).click()
+
         // Fill customer information
+        const firstName = "Peter"
+        const lastName = "Angelo"
+        const postalCode = "112233"
+
+        await (await $('.checkout_info')).waitForDisplayed()
+        await (await $('[data-test="firstName"]')).setValue(firstName)
+        await (await $('[data-test="lastName"]')).setValue(lastName)
+        await (await $('[data-test="postalCode"]')).setValue(postalCode)
+        await (await $('[data-test="continue"]')).click()
+
+
         // Assert final order overview
+        await (await $('.cart_list')).waitForDisplayed()
+        await (await $('.summary_info')).waitForDisplayed()
+        await (await $('[data-test="finish"]')).click()
+
+        const message = await $('.complete-header')
+        await expect(message).toHaveTextContaining('THANK YOU FOR YOUR ORDER')
+
     })
 })
